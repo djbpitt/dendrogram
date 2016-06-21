8d37a93030bb7219c19c01233a1d0a597594d051
David J Birnbaum <djbpitt@gmail.com>
2016-06-21 05:20:53 -0400

    Oops! Forgot to add dev.R after adding loop structure to it

--- dev.R
+++ dev.R
@@ -8,9 +8,16 @@ library(cluster)
 labels<-scan('labels.txt',what='',sep=',')
 # Methods: average, single, complete, ward, weighted 
 #   Omit flexible and gavarage, which are the same as weighted
-clusters = agnes(m,diss=TRUE,method='complete')
-labels<-scan('labels.txt','',sep=',')
-par(cex=0.2)
-svg('complete.svg')
-plot(clusters, labels=labels, which.plots = 2, cex = 0.2, cex.main = 0.5, cex.lab = 0.5, cex.axis = 0.5, cex.sub = 0.5)
-dev.off()
+for (currentMethod in c('average', 'single', 'complete', 'ward', 'weighted')){
+  clusters = agnes(m,diss=TRUE,method=currentMethod)
+  # Write order information to attach links later
+  orderFile<-file(paste(currentMethod,'.order.txt',sep=''))
+  writeLines(toString(unlist(clusters["order"])), orderFile)
+  close(orderFile)
+  # Write svg
+  svg(paste(currentMethod,'.svg',sep=''))
+  plot(clusters, labels=labels, which.plots = 2, cex = 0.2, 
+       cex.main = 0.5, cex.lab = 0.5, cex.axis = 0.5, cex.sub = 0.5, 
+       main = paste('Repertorium corpus,', currentMethod, 'linkage,', size, 'manuscripts'))
+  dev.off()
+}
